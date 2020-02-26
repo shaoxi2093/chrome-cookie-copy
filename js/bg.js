07f5d36c5c4e260a55170c8fca221246d791ff27
shaoxi <shaoxi2093@foxmail.com>
2020-02-26 19:01:27 +0800

    feat: add 0.2版本

--- js/bg.js
+++ js/bg.js
@@ -1,33 +1,41 @@
-window.onload = function() {
-  if (!chrome.cookies) {
-    chrome.cookies = chrome.experimental.cookies;
-  }
-  if (this.location.host == "developer.daily.tuya-inc.cn") {
-    // this.localStorage.setItem('backupCookies', document.cookie)
-    chrome.storage.sync.set({ backupCookies: document.cookie }, function() {
-      console.log("保存成功！")
-    })
-  }
-  this.console.log("chrome cookies=======>>>", chrome.cookies);
-  chrome.cookies.getAll({'domain': '.tuya-inc.cn/'}, (cks) => {
-    this.console.log('chrome cookies=======>>>', cks)
+if (!chrome.cookies) {
+  chrome.cookies = chrome.experimental.cookies;
+}
 
-  })
-  if (this.location.host == "localhost:3000") {
-    // var cookie_backup = this.localStorage.getItem("backupCookies");
-    chrome.storage.sync.get({ backupCookies: "" }, function(items) {
-      var cookie_backup = items.backupCookies;
-      document.cookie = cookie_backup;
-    });
-  }
-  this.console.log("document cookies=====>>>>>", document.cookie);
-  function getCookie(cname) {
-    var name = cname + "=";
-    var ca = document.cookie.split(";");
-    for (var i = 0; i < ca.length; i++) {
-      var c = ca[i].trim();
-      if (c.indexOf(name) == 0) return c.substring(name.length, c.length);
+function filterCookieByFilters({ domain, name }, cookiesArr) {
+  return cookiesArr.find(item => item.domain === domain && item.name === name)
+    .value;
+}
+chrome.cookies.onChanged.addListener(function({ cookie }) {
+  // const s_sid_daily = filterCookieByFilters({domain: 'developer.daily.tuya-inc.cn', name: 's-sid'})
+  if (
+    cookie.domain === "developer.daily.tuya-inc.cn" &&
+    cookie.name === "s-sid"
+  ) {
+    console.log('s-sid=========>>>>', cookie.value)
+    const s_sid_daily = cookie.value;
+    if (s_sid_daily) {
+      chrome.cookies.set(
+        { name: "s-sid", value: s_sid_daily, domain: "localhost", url: 'http://localhost:3000' },
+        () => {
+          console.log("成功");
+        }
+      );
     }
-    return "";
   }
-};
+});
+this.console.log("chrome cookies=======>>>", chrome.cookies);
+chrome.cookies.getAll({ domain: "developer.daily.tuya-inc.cn" }, cks => {
+  this.console.log("chrome cookies=======>>>", cks);
+});
+
+this.console.log("document cookies=====>>>>>", document.cookie);
+function getCookie(cname) {
+  var name = cname + "=";
+  var ca = document.cookie.split(";");
+  for (var i = 0; i < ca.length; i++) {
+    var c = ca[i].trim();
+    if (c.indexOf(name) == 0) return c.substring(name.length, c.length);
+  }
+  return "";
+}
--- js/indexPage.js
+++ js/indexPage.js
@@ -1,3 +1,28 @@
 window.onload = function() {
-  
+  this.document.getElementById('saveSetting').onclick = () => {
+    this.document.querySelector('.msg-error').style.display = 'none'
+    var values = getFormValues()
+    if(!values) {
+      this.document.querySelector('.msg-error').style.display = 'block'
+      return
+    }
+    
+  }
+
+  function getFormValues() {
+    var value_name = this.document.getElementById('name').value
+    var value_origin_url = this.document.getElementById('originUrl').value
+    var value_goat_url = this.document.getElementById('goatUrl').value
+    var value_cookies = this.document.getElementById('goatCookie').value
+
+    if(!value_name || !value_origin_url || !value_goat_url || !value_cookies) {
+      return false
+    }
+    return {
+      name: value_name,
+      originUrl: value_origin_url,
+      goatUrl: value_goat_url,
+      goatCookie: value_cookies,
+    }
+  }
 }
