52d165d3913e7597f3188fa020b3c9c8b8dcc2b7
shaoxi <shaoxi2093@foxmail.com>
2020-02-28 18:34:05 +0800

    feat: add 支持关闭这种实时监听更换

--- js/bg.js
+++ js/bg.js
@@ -7,22 +7,25 @@ function filterCookieByFilters({ domain, name }, cookiesArr) {
     .value;
 }
 chrome.cookies.onChanged.addListener(function({ cookie }) {
-  // const s_sid_daily = filterCookieByFilters({domain: 'developer.daily.tuya-inc.cn', name: 's-sid'})
-  if (
-    cookie.domain === "developer.daily.tuya-inc.cn" &&
-    cookie.name === "s-sid"
-  ) {
-    console.log('s-sid=========>>>>', cookie.value)
-    const s_sid_daily = cookie.value;
-    if (s_sid_daily) {
-      chrome.cookies.set(
-        { name: "s-sid", value: s_sid_daily, domain: "localhost", url: 'http://localhost:3000' },
-        () => {
-          console.log("成功");
+  chrome.storage.sync.get(['openAsync'], ({openAsync}) => {
+    if(openAsync) {
+      if (
+        cookie.domain === "developer.daily.tuya-inc.cn" &&
+        cookie.name === "s-sid"
+      ) {
+        console.log('s-sid=========>>>>', cookie.value)
+        const s_sid_daily = cookie.value;
+        if (s_sid_daily) {
+          chrome.cookies.set(
+            { name: "s-sid", value: s_sid_daily, domain: "localhost", url: 'http://localhost:3000' },
+            () => {
+              console.log("成功");
+            }
+          );
         }
-      );
+      }
     }
-  }
+  })
 });
 this.console.log("chrome cookies=======>>>", chrome.cookies);
 chrome.cookies.getAll({ domain: "developer.daily.tuya-inc.cn" }, cks => {
--- js/indexPage.js
+++ js/indexPage.js
@@ -1,19 +1,41 @@
-window.onload = function() {
-  this.document.getElementById('saveSetting').onclick = () => {
-    this.document.querySelector('.msg-error').style.display = 'none'
-    var values = getFormValues()
-    if(!values) {
-      this.document.querySelector('.msg-error').style.display = 'block'
-      return
+$(document).ready(function() {
+
+  chrome.storage.sync.get(['openAsync'], ({openAsync}) => {
+    if(openAsync) {
+      $('#switchMain').addClass('switch-checked')
+    } else {
+      $('#switchMain').removeClass('switch-checked')
     }
-    
+  })
+
+  $('#switchMain').click(() => {
+    const preValue = $('#switchMain').hasClass('switch-checked')
+    if(preValue) {
+      $('#switchMain').removeClass('switch-checked')
+      $('.mask').addClass('mask-active')
+    } else {
+      $('#switchMain').addClass('switch-checked')
+      $('.mask').removeClass('mask-active')
+    }
+    switchAsync(!preValue)
+  })
+
+  function switchAsync(open = false) {
+    chrome.storage.sync.set({openAsync: open})
+  }
+
+  function resetFormValues() {
+    $('#name').val('')
+    $('#originUrl').val('')
+    $('#goatUrl').val('')
+    $('#goatCookie').val('')
   }
 
   function getFormValues() {
-    var value_name = this.document.getElementById('name').value
-    var value_origin_url = this.document.getElementById('originUrl').value
-    var value_goat_url = this.document.getElementById('goatUrl').value
-    var value_cookies = this.document.getElementById('goatCookie').value
+    var value_name = $('#name').val()
+    var value_origin_url = $('#originUrl').val()
+    var value_goat_url = $('#goatUrl').val()
+    var value_cookies = $('#goatCookie').val()
 
     if(!value_name || !value_origin_url || !value_goat_url || !value_cookies) {
       return false
@@ -25,4 +47,4 @@ window.onload = function() {
       goatCookie: value_cookies,
     }
   }
-}
+})
