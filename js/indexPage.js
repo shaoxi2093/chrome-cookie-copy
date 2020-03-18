935385e34beba0c899ff55fa18f1dd34e0d840d8
shaoxi <shaoxi2093@foxmail.com>
2020-03-18 18:24:58 +0800

    feat: add 新增和展示配置的逻辑

--- js/indexPage.js
+++ js/indexPage.js
@@ -1,112 +1,135 @@
 $(document).ready(function() {
+  initPage();
+  initPageEvent();
 
-  initPage()
-  initPageEvent() 
-
-
-  function initPage () {
-    chrome.storage.sync.get(['openAsync'], ({openAsync}) => {
-      if(openAsync) {
-        $('#switchMain').addClass('switch-checked')
+  function initPage() {
+    chrome.storage.sync.get(["openAsync"], ({ openAsync }) => {
+      if (openAsync) {
+        $("#switchMain").addClass("switch-checked");
       } else {
-        $('#switchMain').removeClass('switch-checked')
+        $("#switchMain").removeClass("switch-checked");
       }
-    })
+    });
+    refreshPagesData()
   }
 
   function refreshPagesData() {
-    chrome.storage.sync.get(['pages'], ({ pages }) => {
-
-    })
+    chrome.storage.sync.get(["pages"], ({ pages }) => {
+      var innerHtmlStr = '<p class="title">同步页面列表</p>';
+      if(pages) {
+        for (key of Object.keys(pages)) {
+          const html = `
+            <div class="pages-item">
+              <div class="item-name">
+                <i data-key="${key}" class="iconfont icon-shuaxin" title="刷新同步"></i>
+                <span class="page-name" title="日常同步">${pages[key].name}</span>
+                <div class="item-opts">
+                  <i data-key="${key}" class="iconfont icon-bianji" title="编辑"></i>
+                  <i data-key="${key}" class="iconfont icon-shanchu" title="删除"></i>
+                </div>
+              </div>
+              <button
+                type="button"
+                class="switch ${
+                  pages[key].switchOn ? "switch-checked" : ""
+                } pages-item-switch"
+              >
+                <span class="switch-inner"></span>
+              </button>
+            </div>
+          `;
+          innerHtmlStr += html
+        }
+      }
+      
+      $('.pages-wrapper').html(innerHtmlStr || `<p class="pages-none">暂无配置任何同步</p>`)
+    });
   }
-
 
   function initPageEvent() {
-    $('#switchMain').click(() => {
-      const preValue = $('#switchMain').hasClass('switch-checked')
-      if(preValue) {
-        $('#switchMain').removeClass('switch-checked')
-        $('.mask').addClass('mask-active')
+    $("#switchMain").click(() => {
+      const preValue = $("#switchMain").hasClass("switch-checked");
+      if (preValue) {
+        $("#switchMain").removeClass("switch-checked");
+        $(".mask").addClass("mask-active");
       } else {
-        $('#switchMain').addClass('switch-checked')
-        $('.mask').removeClass('mask-active')
+        $("#switchMain").addClass("switch-checked");
+        $(".mask").removeClass("mask-active");
       }
-      switchAsync(!preValue)
-    })
+      switchAsync(!preValue);
+    });
 
-    $('#saveSetting').click(() => {
-      const values = getFormValues()
-      if(!values) {
-        return
+    $("#saveSetting").click(() => {
+      const values = getFormValues();
+      if (!values) {
+        return;
       }
-      saveAsync(values, () => {
-        resetFormValues()
-      })
-    })
+      saveAsync({ ...values, switchOn: true }, () => {
+        resetFormValues();
+        refreshPagesData();
+      });
+    });
 
-    $('.pages-item').delegate('.icon-shuaxin', 'click', function(e) {
-      $(this).addClass('icon-shuaxin-round')
-    })
+    $(".pages-item").delegate(".icon-shuaxin", "click", function(e) {
+      $(this).addClass("icon-shuaxin-round");
+    });
 
-    $('.pages-item').on('click', '.pages-item-switch', () => {
+    $(".pages-item").on("click", ".pages-item-switch", () => {});
+    $(".pages-item").on("click", ".icon-bianji", () => {});
+    $(".pages-item").on("click", ".icon-shanchu", () => {});
 
-    })
-  
-    $('#saveSettingCancel').click(() => {
-      resetFormValues()
-      $('.add-card').removeClass('add-card-active')
-    })
-  
-    $('#addNewAsync').click(() => {
-      if($('.add-card').hasClass('add-card-active')) {
-        return
+    $("#saveSettingCancel").click(() => {
+      resetFormValues();
+      $(".add-card").removeClass("add-card-active");
+    });
+
+    $("#addNewAsync").click(() => {
+      if ($(".add-card").hasClass("add-card-active")) {
+        return;
       }
-      resetFormValues()
-      $('.add-card').addClass('add-card-active')
-    })
+      resetFormValues();
+      $(".add-card").addClass("add-card-active");
+    });
   }
 
-  
-
   function saveAsync(values = {}, callback = () => {}) {
-    chrome.storage.sync.get(['pages'], ({pages}) => {
-      if(values.id) {
-        const newPages = { ...pages, [values.id]: values }
-        chrome.storage.sync.set({ pages: newPages })
+    chrome.storage.sync.get(["pages"], ({ pages }) => {
+      if (values.id) {
+        const newPages = { ...pages, [values.id]: values };
+        chrome.storage.sync.set({ pages: newPages });
       } else {
-        const id = 'pageId_' + Math.round(Math.random() * 100000)
-        chrome.storage.sync.set({ pages: {...pages, [id]: values }})
+        const id = "pageId_" + Math.round(Math.random() * 100000);
+        chrome.storage.sync.set({ pages: { ...pages, [id]: values } });
       }
-      callback && callback()
-    })
-    
+      callback && callback();
+    });
   }
 
   function switchAsync(open = false) {
-    chrome.storage.sync.set({openAsync: open})
+    chrome.storage.sync.set({ openAsync: open });
   }
 
-  function resetFormValues() {
-    $('#name').val('')
-    $('#originUrl').val('')
-    $('#goatUrl').val('')
-    $('#goatCookie').val('')
+  function resetFormValues(page = {}) {
+    $("#name").val(page.name || "");
+    $("#originUrl").val(page.originUrl || "");
+    $("#goatUrl").val(page.goatUrl || "");
+    $("#goatCookie").val(page.goatCookie || "");
   }
 
   function getFormValues() {
-    var value_name = $('#name').val()
-    var value_origin_url = $('#originUrl').val()
-    var value_goat_url = $('#goatUrl').val()
-    var value_cookies = $('#goatCookie').val()
+    var value_name = $("#name").val();
+    var value_origin_url = $("#originUrl").val();
+    var value_goat_url = $("#goatUrl").val();
+    var value_cookies = $("#goatCookie").val();
 
-    if(!value_name || !value_origin_url || !value_goat_url || !value_cookies) {
-      return false
+    if (!value_name || !value_origin_url || !value_goat_url || !value_cookies) {
+      return false;
     }
     return {
       name: value_name,
       originUrl: value_origin_url,
       goatUrl: value_goat_url,
-      goatCookie: value_cookies,
-    }
+      goatCookie: value_cookies
+    };
   }
-})
+});
